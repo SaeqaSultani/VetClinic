22dba88a4c6e71b07f7c3a3f15fbeeed735c64ef
Saeqa Sultani <saeqasultani@gmail.com>
2022-11-12 21:23:51 +0430

    Added some data to the visits & specializations & vets tables

--- schema.sql
+++ schema.sql
@@ -33,3 +33,29 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id integer, ADD FOREIGN KEY (species_id) REFERENCES species(id);
 
 ALTER TABLE animals ADD owner_id integer, ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+-- Day4
+
+CREATE TABLE vets (
+    id integer PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    age integer NOT NULL,
+    date_of_graduation date NOT NULL
+);
+
+CREATE TABLE specializations (
+    id integer PRIMARY KEY,
+    species_id integer NOT NULL,
+    vet_id integer NOT NULL,
+    FOREIGN KEY (species_id) REFERENCES species (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
+
+CREATE TABLE visits (
+    id integer PRIMARY KEY,
+    animal_id integer NOT NULL,
+    vet_id integer NOT NULL,
+    date_of_visit date NOT NULL,
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
